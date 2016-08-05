b534413d0342d4ab2786b5cee39b0a74583f08b9
Vaith <vaithwee@yeah.net>
2016-08-05 17:55:40 +0800

    add sms

---
+++ Learning Android/SMSTest/app/src/main/java/com/example/vaith/smstest/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.vaith.smstest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView sender;
+    private TextView content;
+
+    private IntentFilter intentFilter;
+    private MessageReceiver messageReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sender = (TextView) findViewById(R.id.sender);
+        content = (TextView) findViewById(R.id.content);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        messageReceiver = new MessageReceiver();
+        registerReceiver(messageReceiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        unregisterReceiver(messageReceiver);
+    }
+
+    class MessageReceiver extends BroadcastReceiver
+    {
+        @Override
+        public  void onReceive(Context ctx, Intent intent)
+        {
+            Bundle bundle = intent.getExtras();
+            Object[] pdus = (Object[])bundle.get("pdus");
+            SmsMessage[] messages = new SmsMessage[pdus.length];
+            for (int i = 0; i < messages.length; ++i)
+            {
+                messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+
+            }
+
+            String address = messages[0].getOriginatingAddress();
+            String fullMessage = "";
+            for (SmsMessage message: messages)
+            {
+                fullMessage += message.getMessageBody();
+            }
+            sender.setText(address);
+            content.setText(fullMessage);
+        }
+    }
+}
