62b063627dfda9970aea29e5bcd477eb04d34590
Vaith <vaithwee@yeah.net>
2016-08-10 18:06:54 +0800

    add more

---
+++ Android/RatingBar/app/src/main/java/com/example/vaith/ratingbar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.vaith.ratingbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.rating);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+
+            }
+        });
+    }
+}
