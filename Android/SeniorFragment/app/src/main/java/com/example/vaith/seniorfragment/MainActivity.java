12f219437b0434f089f8ef3df805066fed87aa38
Vaith <vaithwee@yeah.net>
2016-08-17 18:01:02 +0800

    add more

---
+++ Android/SeniorFragment/app/src/main/java/com/example/vaith/seniorfragment/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.vaith.seniorfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+    }
+}
